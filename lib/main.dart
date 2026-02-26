4cf325b60ebd713d4a0891eaad381319c7fc0153
Manuel-Campoy <mcampoyteran5@gmail.com>
2026-02-26 09:41:40 -0700

    main y router

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:sistemamine_app/core/di/injection.dart'; // Ruta de inyección
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sistemamine_app/core/di/injection.dart';
+import 'package:get_it/get_it.dart';
+import 'core/constants/app_colors.dart';
+import 'core/router/app_router.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart';
 
-void main() {
-  // Asegura que los bindings de Flutter estén listos antes de arrancar
+void main() async {
+  // Asegura que los bindings de Flutter estén listos
   WidgetsFlutterBinding.ensureInitialized();
   
-  // Inyección de dependencias
   configureDependencies(); 
 
-  runApp(const MyApp());
+  runApp(const SistemaMineApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SistemaMineApp extends StatelessWidget {
+  const SistemaMineApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SistemaMine',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber), 
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('SistemaMine - Infraestructura lista'),
+    // MultiBlocProvider inyecta los "cerebros" en el árbol de widgets
+    return MultiBlocProvider(
+      providers: [
+        // Le pedimos a GetIt que nos dé la instancia del AuthBloc
+        BlocProvider(create: (_) => GetIt.instance<AuthBloc>()),
+      ],
+      child: MaterialApp.router(
+        title: 'SistemaMine',
+        debugShowCheckedModeBanner: false, 
+        theme: ThemeData(
+          scaffoldBackgroundColor: AppColors.gray50,
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryBlue),
+          useMaterial3: true,
         ),
+        routerConfig: appRouter, 
       ),
     );
   }
